c225d4ccac433056f12d3233e2a6404ad843d02e
Yago Amaral <yago.m.amaral@gmail.com>
2023-08-04 13:46:58 -0300

    set SourceLink target to _blank

--- src/components/SourceLink.jsx
+++ src/components/SourceLink.jsx
@@ -3,7 +3,7 @@ import "./css/SourceLink.css";
 function SourceLink() {
     return (
         <div className="source-container">
-            <a href="https://github.com/yago-amaral/yago-amaral.github.io">
+            <a href="https://github.com/yago-amaral/yago-amaral.github.io" target="_blank" rel="noreferrer">
                 Código Fonte
             </a>
         </div>
